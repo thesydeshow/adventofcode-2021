4995e69bc25b63d5481f36e3321698be3bbed2f8
Chris Thevis <christopher.thevis@gmail.com>
2021-12-01 22:42:21 -0600

    reminded about .splice(); learned about .reduce()

--- day01/app.mjs
+++ day01/app.mjs
@@ -26,14 +26,12 @@ const getPositiveDeltaCount = async (data, increment = 1) => {
     const debug = data.length < 10;
     let positiveDeltas = 0;
     for(let i = increment; i < data.length; i++) {
-        let start = 0;
-        let end = 0;
-        for(let j = 0; j < increment; j++) {
-            start += data[i-j-1];
-            end += data[i-j];
-        }
+        const start = data.slice(i-increment, i).reduce((total, addend) => total + addend, 0);
+        const end = data.slice(i-increment+1, i+1).reduce((total, addend) => total + addend, 0);
+
         if(i===increment) debug && console.log(start);
         debug && console.log(end);
+
         if(start < end){
             positiveDeltas++;
             debug && console.log('increased, was', start);
