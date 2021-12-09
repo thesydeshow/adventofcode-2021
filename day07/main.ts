da62fe7167dbd9b64297616852e1ab5f5a689ce0
Chris Thevis <christopher.thevis@gmail.com>
2021-12-09 00:30:07 -0600

    day7 let's gooo!

--- day07/main.ts
+++ day07/main.ts
@@ -4,13 +4,11 @@ function getHorizontalPositions(lines: string[]): Promise<number[]> {
     return Promise.resolve(lines[0].split(',').map(x => Number(x)));
 }
 
-function getMedians(positions: number[]): number[] {
+function getMedians(positions: number[]): number {
     const sortedPositions = positions.sort((a,b) => a - b);
-    const medianIndex1 = Math.floor(positions.length / 2 + 0.5) - 1;
-    const medianIndex2 = Math.ceil(positions.length / 2 + 0.5) - 1;
-    console.log(medianIndex1, medianIndex2);
+    const medianIndex = Math.ceil(positions.length / 2) - 1;
 
-    return [sortedPositions[medianIndex1], sortedPositions[medianIndex2]];
+    return sortedPositions[medianIndex];
 }
 
 function getFuelSpent(positions: number[], target: number): number {
@@ -20,12 +18,38 @@ function getFuelSpent(positions: number[], target: number): number {
     return fuelSpent;
 }
 
+function getExponentialFuelSpent(position: number, target: number): number {
+    const distance = Math.abs(position - target);
+    const fuelSpent = 0.5 * distance * ( distance + 1 );
+    return fuelSpent;
+}
+
+function getTotalFuelSpent(positions: number[], target: number): number {
+    return positions.reduce((previous,current) => previous + getExponentialFuelSpent(current, target), 0);
+}
+
+function getMinTotalFuelSpent(positions: number[], median: number): number {
+    let newGuess = getTotalFuelSpent(positions, median);
+    let previousGuess: number;
+    console.log(median, '=>', newGuess);
+    
+    do {
+        previousGuess = newGuess;
+        median++;
+        newGuess = getTotalFuelSpent(positions, median);
+        console.log(median, '=>', newGuess);
+    } while(previousGuess > newGuess)
+    
+    return previousGuess;
+}
+
 getLinesFromInput('../day07/input.txt').then(result => {
     return getHorizontalPositions(result);
 }).then(result => {
-    const medians = getMedians(result);
-    console.log('medians:', medians);
-    const answer1 = getFuelSpent(result, medians[0]);
-    const answer2 = getFuelSpent(result, medians[1]);
-    console.log('possible answers:', answer1, answer2);
+    const median = getMedians(result);
+    console.log('median:', median);
+    const answer = getFuelSpent(result, median);
+    console.log('answer:', answer);
+    const crabsAnswer = getMinTotalFuelSpent(result, median);
+    console.log('answer:', crabsAnswer);
 })
