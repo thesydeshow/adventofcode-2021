4ce8132ff68a8afffea1bfa24c6ebba8913a5506
Chris Thevis <christopher.thevis@gmail.com>
2021-12-08 23:02:55 -0600

    day6 complete! here comes the math!!

--- day06/main.ts
+++ day06/main.ts
@@ -1,33 +1,41 @@
 import { getLinesFromInput } from '../core/readInput'
 
 function initFish(input: string[]): Promise<number[]>{
-    const fish = input[0].split(',').map(x => Number(x));
+    let fish = Array(9).fill(0);
+    input[0].split(',').map(x => Number(x)).forEach(x => {
+        fish[x]++;
+    })
     return Promise.resolve(fish);
 }
 
 function dayLapse(fish: number[]) {
-    const fishCount = fish.length;
-    for(let i = 0; i < fishCount; i++) {
-        if(fish[i] === 0) {
-            fish[i] = 6;
-            fish.push(8);
-        } else {
-            fish[i]--;
-        }
+    let newFish = fish[0];
+    for(let i = 0; i < fish.length - 1; i++) {
+        fish[i] = fish[i+1];
     }
+    fish[8] = newFish;
+    fish[6] += newFish;
 }
 
 function lapseDays(fish: number[], days: number): Promise<number[]> {
     for(let i = 0; i < days; i++) {
         dayLapse(fish);
     }
-
     return Promise.resolve(fish);
 }
 
-getLinesFromInput('../day06/sample.txt').then(result => {
+function getFishCount(fish: number[]): Promise<number> {
+    const fishCount = fish.reduce((previous,current) => previous + current);
+    return Promise.resolve(fishCount);
+}
+
+getLinesFromInput('../day06/input.txt').then(result => {
     return initFish(result);
 }).then(result => {
-    lapseDays(result, 80);
-    console.log('answer:', result.length);
+    return lapseDays(result, 256);
+}).then(result => {
+    console.log(result);
+    return getFishCount(result);
+}).then(result => {
+    console.log(result);
 })
