30ff2c075a37d0339c4e95756a1385d6446aeca3
Chris Thevis <christopher.thevis@gmail.com>
2021-12-11 22:18:47 -0600

    gg ez

--- day10/main.ts
+++ day10/main.ts
@@ -20,16 +20,46 @@ function syntaxCheck(line: string): number {
                 stack.push(line[i]);
         }
     }
-    return 0;
+    
+    console.log(stack);
+    let incompleteScore = 0;
+    do {
+        incompleteScore *= 5;
+        switch(stack.pop()) {
+            case '(':
+                incompleteScore -= 1;
+                break;
+            case '[':
+                incompleteScore -= 2;
+                break;
+            case '{':
+                incompleteScore -= 3;
+                break;
+            case '<':
+                incompleteScore -= 4;
+                break;
+        }
+        console.log(incompleteScore);
+    } while(stack.length);
+    return incompleteScore;
 }
 
 function part1(inputPath: string) {
     getLinesFromInput(inputPath).then(results => {
-        const syntaxScores = results.map(x => syntaxCheck(x));
+        const syntaxScores = results.map(x => syntaxCheck(x)).filter(x => x > 0);
         console.log('syntaxScores:', syntaxScores);
         const answer = syntaxScores.reduce((p,c) => p + c);
         console.log('answer:', answer);
     })
 }
 
-part1('../day10/input.txt')
+function part2(inputPath: string) {
+    getLinesFromInput(inputPath).then(results => {
+        const syntaxScores = results.map(x => syntaxCheck(x)).filter(x => x < 0);
+        console.log('syntaxScores:', syntaxScores);
+        const answer = syntaxScores.sort((a,b) => a - b)[Math.floor(syntaxScores.length/2)] * -1;
+        console.log('answer:', answer);
+    })
+}
+
+part2('../day10/input.txt')
