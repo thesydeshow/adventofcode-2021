2d8f0aa33827172a632e7f7bc3977d091622cec7
Chris Thevis <christopher.thevis@gmail.com>
2021-12-11 21:35:26 -0600

    day 9 complete

--- day09/main.ts
+++ day09/main.ts
@@ -24,6 +24,41 @@ function getLowestPoints(chart: number[][]): number[] {
     return lowestPoints;
 }
 
+function mapBasin(x: number, y: number, chart: [number,boolean][][]): number {
+    if(chart[x][y][0] >= 9 || chart[x][y][1]) {
+        return 0;
+    }
+
+    let i: number = 1;
+    chart[x][y][1] = true;
+
+    if(x > 0 && chart[x-1][y][0] < 9 && !chart[x-1][y][1]) {
+        i += mapBasin(x-1, y, chart);
+    }
+    if(x < chart.length-1 && chart[x+1][y][0] <= 9 && !chart[x+1][y][1]) {
+        i += mapBasin(x+1, y, chart);
+    }
+    if(y > 0 && chart[x][y-1][0] < 9 && !chart[x][y-1][1]) {
+        i += mapBasin(x, y-1, chart);
+    }
+    if(y < chart[x].length-1 && chart[x][y+1][0] < 9 && !chart[x][y+1][1]) {
+        i += mapBasin(x, y+1, chart);
+    }
+
+    return i;
+}
+
+function getBasinSizes(chart: number[][]): number[] {
+    let basinSizes: number[] = [];
+    for(let i = 0; i < chart.length; i++) {
+        for(let j = 0; j < chart[i].length; j++) {
+            if(isLowest(i, j, chart)) basinSizes.push(mapBasin(i, j, chart.map(x => x.map(y => [y,false]))));
+        }
+    }
+
+    return basinSizes;
+}
+
 function part1(inputPath: string) {
     getLinesFromInput(inputPath).then(result => {
         const depthChart = getDepthChart(result);
@@ -35,4 +70,14 @@ function part1(inputPath: string) {
     })
 }
 
-part1('../day09/input.txt')
+function part2(inputPath: string) {
+    getLinesFromInput(inputPath).then(result => {
+        const depthChart = getDepthChart(result);
+        const basinSizes = getBasinSizes(depthChart);
+        console.log('basinSizes:', basinSizes);
+        const answer = basinSizes.sort((a,b) => b - a).filter((x,i) => i < 3).reduce((p,c) => p * c, 1);
+        console.log('answer:', answer);
+    })
+}
+
+part2('../day09/input.txt')
