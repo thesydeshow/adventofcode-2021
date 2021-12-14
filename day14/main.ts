59fba77016fe07a644af0d130f91fcabbcc52507
Chris Thevis <christopher.thevis@gmail.com>
2021-12-14 00:24:24 -0600

    day 14 setup

---
+++ day14/main.ts
@@ -0,0 +1,8 @@
+import { getLinesFromInput } from '../core/readInput';
+import { join } from 'path';
+
+function part1(inputFilename: string) {
+    getLinesFromInput(join('..', 'day14', inputFilename)).then(result => {
+        
+    });
+}
