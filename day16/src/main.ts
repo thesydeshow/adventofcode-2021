1c74ec3b4d8271100e5e56c566f6a5f45baecea8
Chris Thevis <christopher.thevis@gmail.com>
2021-12-18 19:22:07 -0600

    fixed bug, all tests passing

--- day16/src/main.ts
+++ day16/src/main.ts
@@ -1,11 +1,15 @@
 import { LiteralPacket, OperatorPacket, Packet, PacketLengthError, PacketTypeError } from './classes';
 
-function getBits(hex: string): string {
+function getSingleBits(hex: string): string {
     const bits = parseInt(hex, 16).toString(2);
 
     if(bits.length % 4 === 0) return bits;
     // this will have stripped off leading zeros, so need to add them back on up to 4 digits
     return '0000'.substring(bits.length % 4) + bits;
+}
+
+function getBits(hex: string): string {
+    return hex.split('').reduce((p,c) => p + getSingleBits(c), '');
 }
 
 function initLiteralValue(packet: ILiteralValuePacket) {
