277b08fe9a922c16321d0da239ef6adc7f161d18
Chris Thevis <christopher.thevis@gmail.com>
2021-12-19 10:12:36 -0600

    a little too easy...

--- day17/src/main.ts
+++ day17/src/main.ts
@@ -1,7 +1,35 @@
+function getMaxHeight(minY: number): number {
+    /*
+        Our parabola's formula is...
+               y = -.5x^2 - .5x  (not accounting for deltas in x and y to pass through 0,0 at various slopes)
+        meaning our first derivative is...
+              y' = -x - .5
+        We want to find the point where the derivative is equal to the change in our starting y (0) and the bottom left corner's y (minY), meaning...
+            minY = -x - .5, or
+               x = -.5 - minY
+        This x is the highest x value where the delta in y will occur, and since we need x to be an int, we will Math.floor it...
+               x = floor(-.5 - minY)
+        Plugging x into the original parabola's formula will get us the distance dropped, which we can negate and get the max height possible.
+    */
+   const x = Math.floor(-.5 - minY);
+   const maxHeight = .5 * Math.pow(x, 2) + .5 * x;
+   return maxHeight;
+}
+
+function parseInput(input: string): number[][] {
+    const matches = input.match(/x=(\d+)\.\.(\d+), y=(\-?\d+)\.\.(\-?\d+)/) || [];
+    if(matches.length < 5) throw new Error(`Invalid input: ${input}`);
+
+    return [[Number(matches[1]), Number(matches[3])], [Number(matches[2]), Number(matches[4])]];
+}
+
 function part1(input: string): number {
-    throw new Error('Not implemented');
+    const minAndMaxPoints = parseInput(input);
+    const answer = getMaxHeight(minAndMaxPoints[0][1]);
+    return answer;
 }
 
 export {
+    getMaxHeight,
     part1
 }
--- day17/test/main.test.ts
+++ day17/test/main.test.ts
@@ -1,12 +1,23 @@
-import { part1 } from '../src/main';
+import { getMaxHeight, part1 } from '../src/main';
 
 const part1Samples = [
-    ['target area: x=20..30, y=-10..-5', 45]
+    ['target area: x=20..30, y=-10..-5', 45],
+    ['target area: x=253..280, y=-73..-46', 2628]
 ]
 
+describe('max height tests', () => {
+    test.each([[-10, 45], [0,0], [-1,0], [-2, 1], [-73,2628]])('given %p as min y, returns max height of %p', (argument, expectedResult) => {
+        const result = getMaxHeight(argument);
+        expect(result).toEqual(expectedResult);
+    })
+})
+
 describe('part 1 samples', () => {
-    test.each(part1Samples)('given %p as part 1 input, returns max y of %p', (argument, expectedResult) => {
+    test.each(part1Samples)('given %p as part 1 input, returns max height of %p', (argument, expectedResult) => {
         const result = part1(<string>argument);
         expect(result).toEqual(expectedResult);
+    })
+    test('given invalid input as part 1 input, throws error', () => {
+        expect(() => {part1('target area: y=20..30, x=-10..-5')}).toThrow();
     })
 })
