a9b0647639ab366db604e32476d60cd034fbc6d6
Chris Thevis <christopher.thevis@gmail.com>
2021-12-19 19:31:14 -0600

    littered my code with math notes

--- day17/src/main.ts
+++ day17/src/main.ts
@@ -34,16 +34,63 @@ function part1(input: string): number {
 }
 
 function getBoundVelocityWithMostSteps(target: number): number {
-    //a little quadradic formulaing
+    /*
+        We want to find the max (or min) number of steps it would take for within the bounds of the target window when we sum up:
+            1 + 2 + 3 + ... + n-2 + n-1 + n
+        So we break out Gauss's formula yet again!
+            target = 0.5 * n * (1 + n)
+        But this time we need to solve for `n`.
+
+        If you remember high school, you remember when presented with a formula:
+            0 = ax^2 + bx + c
+        ...you solve for `x` with the quadratic formula:
+            x = (-b +- sqrt(b^2 - 4ac)) / 2
+        
+        We have the formula (simplified from earlier):
+            target = 0.5*x^2 + 0.5*x
+        which we can rewrite into:
+            0 = x^2 + x - 2(target)
+        which means:
+            a = 1
+            b = 1
+            c = -2 * target
+        which results in:
+            x = (-1 + sqrt(1^2 - 4(1)(-2 * target))) / 2(1)
+        or simplified...
+    */
     return (-1 + Math.pow(1 + 8 * target, 0.5)) / 2;
 }
 
 function getBoundVelocity(target: number, step: number): number {
-    //TODO: show the maths
+    /*
+        This started from Gauss's formula:
+            sum = 0.5 * (x1 - x0 + 1) * (x1 + x0)
+        Since we know the sum (either the min or the max bounds of the target window),
+        and we know the number of steps (since we iterating by velocities that take 1 step, velocities that take 2 steps, etc),
+        we can rewrite the formula as such:
+            minOrMax = 0.5 * n * ((x0 - n + 1) + x0)
+        where `n` is the step number we are on, `x0` is the starting velocity as it goes down 1 per step - down to `x0 - n + 1`.
+
+        Solving for x0, we get:
+            x0 = minOrMax / n + 0.5 * n - 0.5
+        
+        If we're passing in the min x or y, we're going to Math.ceil() the result to stay within the target window.
+        Similarly, if passing in the max, we're going to Math.floor() the result.
+    */
     return target / step + 0.5 * step - 0.5;
 }
 
 function getUpperBoundVelocity(target: number, step: number, cap: boolean): number {
+    /*
+        Cap vs no-cap: for horizontal velocity, the velocity does not degrade lower than 0, so we want to cap it off there at 0.
+        But it's the same to cap it off at 1 since it saves us one operation and gives us the same sum.
+        
+        In other words, instead of having starting the resulting `x` for step n=12 and starting at a velocity of 7 be:
+            x = 7 + 6 + 5 + 4 + 3 + 2 + 1 + 0 + -1 + -2 + -3 + -4 = 18
+        ...which does not fall within the target window of 20 <= x <= 30, but since v=7 _is_ within the range when capped:
+            x = 7 + 6 + 5 + 4 + 3 + 2 + 1 + 0 +  0 +  0 +  0 +  0 = 28
+        ...we need to have it use the cap (floor of getBoundVelocityWithMostSteps(30) => 7).
+    */
     let capAt = 0;
     if(cap && step >= (capAt = Math.floor(getBoundVelocityWithMostSteps(target)))) {
         return capAt;
@@ -62,6 +109,21 @@ function getLowerBoundVelocity(target: number, step: number, cap: boolean) {
 }
 
 function getFeasibleHorizontalVelocities(minX: number, maxX: number, step: number): number[] {
+    /*
+        So I originally thought I would just grab the number feasible starting points for X and Y velocity where:
+            min <= x + x-1 + x-2 + ... x-(n-2) + x-(n-1) <= max
+        or...
+            min <= x + x-1 + x-2 + ... 2 + 1 <= max  [for horizontal velocity, since that will not decrease past 0]
+        Where `min` is the left/top bounds of the target window, `max` is the right/bottom bounds, `n` is the step number, and `x` is a starting velocity.
+        
+        And multiply the possible starting x velocities and y velocities for each step number (up to the max y velocity, similar to what was solved for in part 1).
+
+        But it turns out there were multiple starting velocities that would land in the target window at consecutive steps.
+        For example, starting velocity (8,0) would land in the sample's target window (x=20..30, y=-10..-5) at [26,-6] and [30,-10].
+        
+        I had to refactor the functions to return the possible starting velocities for that axis for the current step.
+        Then do a cartesian join of the two sets of the current step, then distinct the new set.
+    */
     const top = getUpperBoundVelocity(maxX, step, true);
     const bottom = getLowerBoundVelocity(minX, step, true);
     return Array.from(Array(top-bottom+1), (x,i) => i + bottom);
@@ -74,6 +136,13 @@ function getFeasibleVerticalVelocities(minY: number, maxY: number, step: number)
 }
 
 function getMaxSteps(minY: number): number {
+    /*
+        If the max vertical velocity is x:
+        - then it will take x+1 steps to reach the apex
+        - and another x steps to reach y=0
+        - and 1 more step to reach the bottom of the target window
+            maxN = x + 1 + x + 1 = 2x + 2
+    */
     return getMaxVerticalVelocity(minY) * 2 + 2;
 }
 
@@ -105,7 +174,9 @@ function part2(input: string): number {
     return answer;
 }
 
-console.log(part2('target area: x=20..30, y=-10..-5'));
+console.time();
+console.log(part2('target area: x=253..280, y=-73..-46'));
+console.timeEnd();
 
 export {
     getMaxHeight,
