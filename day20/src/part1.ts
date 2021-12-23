84a49e257a757b1d728588ade1db78e891da79dd
Chris Thevis <christopher.thevis@gmail.com>
2021-12-22 23:12:59 -0600

    day 20 setup

---
+++ day20/src/part1.ts
@@ -0,0 +1,7 @@
+import { getLinesFromInput } from '../../core/readInput';
+
+getLinesFromInput('../assets/sample.txt').then(lines => {
+    console.time();
+    console.log('answer:', );
+    console.timeEnd();
+})
